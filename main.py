6b5e33d81f930b943dea775e17f6910cf3f71df8
gdfrh <1291136307@qq.com>
2024-12-15 21:57:36 +0800

    第二次提交

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Map import *
 # 创建一个 10x10 的车间地图
 workshop = MapSpace(map_rows, map_cols,map_fill_char,road_width,work_width_up,work_width_down,work_name_up,work_name_down)
 
+
 # 打印车间地图
 workshop.display()
 
